04c03360d46556874fce0f46bf7b3de13c6c027d
Matthew Chess <mchess@matthews-imac.att.net>
2014-06-07 23:08:31 -0400

    Initial Commit

---
+++ blockout/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  blockout
+//
+//  Created by Matthew Chess on 6/7/14.
+//  Copyright (c) 2014 matthewchess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ blockoutTests/blockoutTests.m
@@ -0,0 +1,34 @@
+//
+//  blockoutTests.m
+//  blockoutTests
+//
+//  Created by Matthew Chess on 6/7/14.
+//  Copyright (c) 2014 matthewchess. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface blockoutTests : XCTestCase
+
+@end
+
+@implementation blockoutTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
